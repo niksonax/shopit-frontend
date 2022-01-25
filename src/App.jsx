72ac78e1f33b968d1d6ca25650309b62892e4379
niksonax <nik2142ua@gmail.com>
2022-01-25 15:53:09 +0200

    Create styled theme and wrap app with ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.scss';
+import { ThemeProvider } from '@mui/material';
 import { Header } from './components';
 import { Routing } from './routing/Routing';
+import mainTheme from './shared/styles/mainTheme';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Routing />
-    </div>
+    <ThemeProvider theme={mainTheme}>
+      <div className="app">
+        <Header />
+        <Routing />
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/shared/styles/mainTheme.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+const mainTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#2e1aad',
+      contrastText: 'red',
+    },
+  },
+});
+
+export default mainTheme;
