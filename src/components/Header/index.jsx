61245608e3f09fec1427df7e23ea7d15b7d69803
niksonax <nik2142ua@gmail.com>
2022-01-25 15:49:30 +0200

    Create Header component and move to scss

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,34 @@
+import './styles.scss';
 import React from 'react';
+import { Box, AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../../routing/routes';
 
 function Header() {
-  return <div>header</div>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" className="header">
+        <Toolbar>
+          <Typography className="logo">ShopIt</Typography>
+          <Box className="nav-links">
+            <Link to={ROUTES.HOME}>
+              <Typography>Home</Typography>
+            </Link>
+            <Typography>Catalog</Typography>
+            <Typography>About</Typography>
+          </Box>
+          <Box className="auth-buttons">
+            <Link to={ROUTES.AUTH} state={{ page: 'login' }}>
+              <Button variant="outlined">Login</Button>
+            </Link>
+            <Link to={ROUTES.AUTH} state={{ page: 'sign-up' }}>
+              <Button variant="contained">Sign Up</Button>
+            </Link>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
 export default Header;
--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
-function Auth() {
-  return <div>Auth</div>;
+function Auth(props) {
+  const location = useLocation();
+
+  const [pageState, setPageState] = useState('sign-up');
+
+  useEffect(() => {
+    if (location.state.page) {
+      setPageState(location.state.page);
+    }
+  }, [location]);
+
+  return <div>Auth {pageState}</div>;
 }
 
 export default Auth;
