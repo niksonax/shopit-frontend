39b44319a2dea8335b0fd0c35291145e766e4a3c
niksonax <nik2142ua@gmail.com>
2022-01-26 11:56:01 +0200

    Update main theme Add basic font family Add colors to palette

--- src/shared/styles/mainTheme.js
+++ src/shared/styles/mainTheme.js
@@ -3,9 +3,17 @@ import { createTheme } from '@mui/material/styles';
 const mainTheme = createTheme({
   palette: {
     primary: {
-      main: '#2e1aad',
-      contrastText: 'red',
+      main: '#2E1AAD',
     },
+    common: {
+      grey: '#999999',
+    },
+    background: {
+      default: '#F5F7FA',
+    },
+  },
+  typography: {
+    fontFamily: "'Manrope', sans-serif",
   },
 });
 
