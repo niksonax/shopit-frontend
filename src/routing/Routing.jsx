59f75dcc8b6e294a38e213d9228d33571e2961d2
niksonax <nik2142ua@gmail.com>
2022-01-28 18:38:14 +0200

    Wrap Routes with AuthProvider and configure private route

--- src/routing/Routing.jsx
+++ src/routing/Routing.jsx
@@ -1,12 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
+import { Auth, Catalog, Home } from '../pages';
+import { AuthProvider } from '../shared/hooks';
+import { RequireAuth } from './RequireAuth';
 import { ROUTES } from './routes';
-import { Auth, Home } from '../pages';
 
 export function Routing() {
   return (
-    <Routes>
-      <Route exact path={ROUTES.HOME} element={<Home />} />
-      <Route exact path={ROUTES.AUTH} element={<Auth />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route exact path={ROUTES.HOME} element={<Home />} />
+        <Route exact path={ROUTES.AUTH} element={<Auth />} />
+        <Route
+          exact
+          path={ROUTES.CATALOG}
+          element={
+            <RequireAuth>
+              <Catalog />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </AuthProvider>
   );
 }
