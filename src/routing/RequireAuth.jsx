6029e8ac73f37e96f46654250d93f092dc43945f
niksonax <nik2142ua@gmail.com>
2022-02-03 18:08:48 +0200

    Try to relogin user using access token

--- src/routing/RequireAuth.jsx
+++ src/routing/RequireAuth.jsx
@@ -1,4 +1,6 @@
-import { Navigate, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useLoginByTokenMutation } from '../shared/api/auth';
 import { useAuth } from '../shared/hooks';
 import { ROUTES } from './routes';
 import { STATES as AUTH_STATES } from '../pages/Auth/constants';
@@ -6,17 +8,24 @@ import { STATES as AUTH_STATES } from '../pages/Auth/constants';
 export function RequireAuth({ children }) {
   const isAuthenticated = useAuth();
   const location = useLocation();
+  const navigate = useNavigate();
 
-  if (!isAuthenticated) {
-    // Save current location to redirect user there after login
-    return (
-      <Navigate
-        to={ROUTES.AUTH}
-        state={{ from: location, page: AUTH_STATES.LOGIN }}
-        replace
-      />
-    );
-  }
+  const [loginByToken] = useLoginByTokenMutation();
+
+  const accessToken = window.localStorage.getItem('accessToken');
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      if (accessToken) {
+        loginByToken();
+      } else {
+        navigate(ROUTES.AUTH, {
+          replace: true,
+          state: { from: location, page: AUTH_STATES.LOGIN },
+        });
+      }
+    }
+  }, [accessToken, isAuthenticated, loginByToken, navigate, location]);
 
   return children;
 }
