1b78ba6902b1bf7331f72dd4a9d04389fe466eb5
niksonax <nik2142ua@gmail.com>
2022-02-04 15:19:53 +0200

    Display products using Grid and ProductCard components

--- src/pages/Catalog/index.jsx
+++ src/pages/Catalog/index.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container, Grid } from '@mui/material';
+import { styled } from '@mui/system';
+import ProductCard from '../../components/ProductCard';
 import { useGetProductsQuery } from '../../shared/api/products';
 
 function Catalog() {
-  const { data, error } = useGetProductsQuery();
+  const { data, error, isLoading } = useGetProductsQuery();
+  const [products, setProducts] = useState([]);
 
-  return <div>It's a catalog</div>;
+  const productCards = products.map((item) => (
+    <Grid item key={item.id}>
+      <ProductCard
+        name={item.name}
+        price={item.price}
+        description={item.description}
+      />
+    </Grid>
+  ));
+
+  useEffect(() => {
+    if (!isLoading) {
+      setProducts(data.products);
+    }
+  }, [data, isLoading]);
+
+  return (
+    <CatalogContainer>
+      <Grid container justifyContent="space-evenly" rowSpacing={5}>
+        {productCards}
+      </Grid>
+    </CatalogContainer>
+  );
 }
+
+const CatalogContainer = styled(Container)(() => ({
+  marginTop: '1rem',
+}));
 
 export default Catalog;
